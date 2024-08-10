5c64f37b93858863c83d4da5e65b2c45057783f7
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-08-10 14:02:47 -0300

    reorganize server.rb code and move csv reading logic to csv_service.rb

--- server.rb
+++ server.rb
@@ -1,26 +1,12 @@
 require 'sinatra'
 require 'rack/handler/puma'
-require 'csv'
+require './app/services/csv_service'
+
+CSV_PATH = './storage/data.csv'.freeze
 
 get '/tests' do
-  rows = CSV.read("./data.csv", col_sep: ';')
-
-  columns = rows.shift
-
-  rows.map do |row|
-    row.each_with_object({}).with_index do |(cell, acc), idx|
-      column = columns[idx]
-      acc[column] = cell
-    end
-  end.to_json
+  content_type :json
+  CsvService.read_csv(CSV_PATH).to_json
 end
 
-get '/hello' do
-  'Hello world!'
-end
-
-Rack::Handler::Puma.run(
-  Sinatra::Application,
-  Port: 3000,
-  Host: '0.0.0.0'
-)
+Rack::Handler::Puma.run Sinatra::Application, Port: 3000, Host: '0.0.0.0'
