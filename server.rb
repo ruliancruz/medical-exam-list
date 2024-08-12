d7477b65dafd679248a9faa75142e6aa1c0c2dfb
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-08-12 08:04:53 -0300

    add rake tasks import data from csv to database and configure server.rb to not run application when in test environment

--- server.rb
+++ server.rb
@@ -1,12 +1,14 @@
 require 'sinatra'
 require 'rack/handler/puma'
-require './app/services/csv_service'
+require './app/services/exam_service'
 
 CSV_PATH = './storage/data.csv'.freeze
 
 get '/tests' do
   content_type :json
-  CsvService.read_csv(CSV_PATH).to_json
+  CSVService.read_csv(CSV_PATH).to_json
 end
 
-Rack::Handler::Puma.run Sinatra::Application, Port: 3000, Host: '0.0.0.0'
+unless ENV['RACK_ENV'] == 'test'
+  Rack::Handler::Puma.run Sinatra::Application, Port: 3000, Host: '0.0.0.0'
+end
