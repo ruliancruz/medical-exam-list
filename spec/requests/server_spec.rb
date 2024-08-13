5e87210b6afce96bbcbb5bebced4c8b98ffb3447
Rulian de Jesus Cruz <rulian.c@protonmail.ch>
2024-08-13 05:28:50 -0300

    modify test matches new desired json response on get /tests endpoint

--- spec/requests/server_spec.rb
+++ spec/requests/server_spec.rb
@@ -19,27 +19,32 @@ RSpec.describe 'Server' do
       get '/tests'
 
       expect(last_response).to be_ok
+
       json = JSON.parse last_response.body
+      first_patient = json.first['patient']
+      first_doctor = json.first['doctor']
+      first_exam = json.first['exam']
+
       expect(json.length).to eq 2
-      expect(json.first['patient_cpf']).to eq '048.973.170-88'
-      expect(json.first['patient_name']).to eq 'Emilly Batista Neto'
-      expect(json.first['patient_email']).to eq 'gerald.crona@ebert-quigley.com'
-      expect(json.first['patient_birthdate']).to eq '2001-03-11'
-      expect(json.first['patient_address']).to eq '165 Rua Rafaela'
-      expect(json.first['patient_city']).to eq 'Ituverava'
-      expect(json.first['patient_state']).to eq 'Alagoas'
-      expect(json.first['doctor_crm']).to eq 'B000BJ20J4'
-      expect(json.first['doctor_state']).to eq 'PI'
-      expect(json.first['doctor_name']).to eq 'Maria Luiza Pires'
-      expect(json.first['doctor_email']).to eq 'denna@wisozk.biz'
-      expect(json.first['exam_result_token']).to eq 'IQCZ17'
-      expect(json.first['exam_result_date']).to eq '2021-08-05'
-      expect(json.first['exam_type']).to eq 'hemácias'
-      expect(json.first['exam_limits']).to eq '45-52'
-      expect(json.first['exam_result']).to eq '97'
-      expect(json.last['patient_cpf']).to eq '048.108.026-04'
-      expect(json.last['doctor_crm']).to eq 'B0002IQM66'
-      expect(json.last['exam_result_token']).to eq '0W9I67'
+      expect(json.first['token']).to eq 'IQCZ17'
+      expect(json.first['date']).to eq '2021-08-05'
+      expect(first_patient['cpf']).to eq '048.973.170-88'
+      expect(first_patient['name']).to eq 'Emilly Batista Neto'
+      expect(first_patient['email']).to eq 'gerald.crona@ebert-quigley.com'
+      expect(first_patient['birthdate']).to eq '2001-03-11'
+      expect(first_patient['address']).to eq '165 Rua Rafaela'
+      expect(first_patient['city']).to eq 'Ituverava'
+      expect(first_patient['state']).to eq 'Alagoas'
+      expect(first_doctor['crm']).to eq 'B000BJ20J4'
+      expect(first_doctor['state']).to eq 'PI'
+      expect(first_doctor['name']).to eq 'Maria Luiza Pires'
+      expect(first_doctor['email']).to eq 'denna@wisozk.biz'
+      expect(first_exam['type']).to eq 'hemácias'
+      expect(first_exam['limits']).to eq '45-52'
+      expect(first_exam['result']).to eq '97'
+      expect(json.last['token']).to eq '0W9I67'
+      expect(json.last['patient']['cpf']).to eq '048.108.026-04'
+      expect(json.last['doctor']['crm']).to eq 'B0002IQM66'
     end
 
     it 'returns a empty array if database has no registered test' do
@@ -57,7 +62,6 @@ RSpec.describe 'Server' do
 
       get '/tests'
 
-      puts DatabaseConnectionManager.use_connection.exec('SELECT version();').to_a
       expect(last_response.status).to eq 503
       expect(JSON.parse(last_response.body)['error'])
         .to include 'Database table not found'
